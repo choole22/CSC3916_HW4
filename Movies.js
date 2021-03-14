fd6f83e7f07fbbc0abfc9c984355d767037e65c9
Chris Hoole <77997980+choole22@users.noreply.github.com>
2021-03-13 18:38:11 -0700

    Add files via upload

---
+++ Movies.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+mongoose.Promise = global.Promise;
+
+//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
+
+//movie schema
+var MovieSchema = new Schema({
+    Title: String,
+    Genre: String,
+    Year:  String,
+    Actor_1: String,
+    Actor_2: String,
+    Actor_3: String
+});
+
+MovieSchema.pre('save', function(next) {
+    var movie = this;
+});
+
+
+}
+
+//return the model to server
+module.exports = mongoose.model('User', UserSchema);
