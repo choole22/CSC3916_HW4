d4b9c6fb99ad4244384ec18ea372a9221fb715b4
Chris Hoole <77997980+choole22@users.noreply.github.com>
2021-03-14 14:06:54 -0600

    Update server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ router.post('/signin', function (req, res) {
 router.get('/movies', function (req, res) {
     console.log(req.body);
     var getMovie = Movie.findOne(req.body.title);
-    res.json(getMovie);
+    res.json(getMovie.Title);
 });
 
 router.post('/movies', function (req, res) {
