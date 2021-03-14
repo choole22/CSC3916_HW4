22532211c89f6e07e09faf9dba09678c5818ff4c
Chris Hoole <77997980+choole22@users.noreply.github.com>
2021-03-13 20:12:59 -0700

    Update Actors.js

--- Actors.js
+++ Actors.js
@@ -1,6 +1,28 @@
-class Actor{
-    constructor(ActorName, CharacterName){
-        this.ActorName = null;
-        this.CharacterName = null;
-    }
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+
+mongoose.Promise = global.Promise;
+
+//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
 }
+mongoose.set('useCreateIndex', true);
+
+//movie schema
+var MovieSchema = new Schema({
+    ActorName: String,
+    CharacterName: String
+});
+
+MovieSchema.pre('save', function(next) {
+    return next();
+});
+
+//return the model to server
+module.exports = mongoose.model('Movie', MovieSchema);
