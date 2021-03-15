7e5708d387c1a39e2184064e80bbd7f2a2b75f59
Chris Hoole <77997980+choole22@users.noreply.github.com>
2021-03-14 18:04:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -133,19 +133,20 @@ router.post('/movies', function (req, res) {
 
 router.route('/movies')
     .delete(authController.isAuthenticated, function(req, res) {
-            console.log(req.body);
-            Movie.findOneAndDelete({Title: req.body.title}, function (err, movies){
+        console.log(req.body);
+        Movie.findOneAndDelete({Title: req.body.title}, function (err, movies){
                 if(err) {res.send(err);}
                 res = res.status(200);
                 res.json({msg: 'Movie Deleted'})
+            )
 
-            if (req.get('Content-Type')) {
-                res = res.type(req.get('Content-Type'));
+                if (req.get('Content-Type')) {
+                    res = res.type(req.get('Content-Type'));
+                }
+                var o = getJSONObjectForMovieRequirement(req);
+                res.json(o);
             }
-            var o = getJSONObjectForMovieRequirement(req);
-            res.json(o);
-        }
-    )
+        )
     .put(authJwtController.isAuthenticated, function(req, res) {
             console.log(req.body);
             res = res.status(200);
